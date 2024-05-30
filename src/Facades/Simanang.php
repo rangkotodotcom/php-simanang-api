07aeb57d3e0e9afe65dd59be47251eab55b4ea00
rangkotodotcom <softj4m98@gmail.com>
2024-05-30 19:50:47 +0700

    update facade

--- src/Facades/Simanang.php
+++ src/Facades/Simanang.php
@@ -5,8 +5,16 @@ namespace Rangkotodotcom\Simanang\Facades;
 use Illuminate\Support\Facades\Facade;
 
 /**
- * Class Simanang
- * @package Rangkotodotcom\Simanang\Facades
+ * @method static \Rangkotodotcom\Simanang getSchool()
+ * @method static \Rangkotodotcom\Simanang getVision()
+ * @method static \Rangkotodotcom\Simanang getMision()
+ * @method static \Rangkotodotcom\Simanang getGallery()
+ * @method static \Rangkotodotcom\Simanang getHeadMaster()
+ * @method static \Rangkotodotcom\Simanang getCurrentSemester()
+ * @method static \Rangkotodotcom\Simanang getStudent(string $param = null, array $data)
+ * @method static \Rangkotodotcom\Simanang getTeacher(string $param = null, array $data)
+ *
+ * @see \Rangkotodotcom\Simanang
  */
 class Simanang extends Facade
 {
